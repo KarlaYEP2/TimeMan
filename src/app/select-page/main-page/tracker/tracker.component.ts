494e994a06f1aed72f8fe4a9dfe14034c8f9c3d9
KarlaYEP2 <karl-erik.voll@khk.ee>
2022-11-27 20:41:07 +0200

    bug: fix behaviour where tracker would calculate every project's hours spent accumulatively instead of specific project

--- src/app/select-page/main-page/tracker/tracker.component.ts
+++ src/app/select-page/main-page/tracker/tracker.component.ts
@@ -13,15 +13,19 @@ export class TrackerComponent implements OnInit {
   listData:Listing[] = [];
   sub: Subscription | undefined
 
+  @Input() projectId: string
   @Input()hoursMax!: number
   hoursSpent!: number
   constructor(private listService:ListService) { }
 
   ngOnInit(): void {
+    const projectId = (this.projectId)
     this.sub = this.listService.listingUpdated.subscribe(
       (arrayData: Listing[]) => {
-        this.listData = arrayData
-        //This function is redundant and there needs to be a better way to present it
+        this.listData = this.listData.filter(function( obj ) {
+          return obj.projectId == projectId
+        });
+        // There needs to be a better way to present it since it is used multiple times
         this.hoursSpent = this.listData.reduce((accumulator, obj) => {
           return accumulator + Number(obj.hours);
         }, 0)
